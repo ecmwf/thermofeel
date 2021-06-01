f712c9def8661f3b9afdb672ba4ed1a5eb0de53c
Tiago Quintino <tiago.quintino@ecmwf.int>
2021-06-01 13:43:09 +0100

    New decoding

--- Postprocessing.py
+++ Postprocessing.py
@@ -5,10 +5,8 @@ import numpy as np
 import pandas as pd
 from ThermoFeel import *
 
-def decode_singleparam(fpath):
-    values_list = []
-    lat_list = []
-    lon_list = []
+def decode_grib(fpath):
+    messages = []
     i = 0
     with open(fpath, "rb") as f:
         while True:
@@ -19,80 +17,49 @@ def decode_singleparam(fpath):
             i += 1
             print("message %d ----------------------------------", i)
 
-            # print metadata key-values
-            it = eccodes.codes_keys_iterator_new(msg, 'ls')
-            while eccodes.codes_keys_iterator_next(it):
-                k = eccodes.codes_keys_iterator_get_name(it)
-                v = eccodes.codes_get_string(msg, k)
-                print("%s = %s" % (k, v))
-            eccodes.codes_keys_iterator_delete(it)
+            md = dict()
 
-            # get the lats, lons, values
-            lats = eccodes.codes_get_double_array(msg, "latitudes")
-            # print(lats)
-            lons = eccodes.codes_get_double_array(msg, "longitudes")
-            # print(lons)
-            values = eccodes.codes_get_double_array(msg, "values")
-            # print(values)
-            lat_list.append(lats)
-            lon_list.append(lons)
-            values_list.append(values)
-            eccodes.codes_release(msg)
+            # decode metadata
 
-    f.close()
-    return values_list,lat_list,lon_list
-
-
-def decode_csza(fpath):
-    values_list = []
-    lat_list = []
-    long_list = []
-    i = 0
-    with open(fpath, "rb") as f:
-        while True:
-            msg = eccodes.codes_any_new_from_file(f)
-            if msg is None:
-                break
-
-            i += 1
-            print("message %d ----------------------------------", i)
-
-            # print metadata key-values
             it = eccodes.codes_keys_iterator_new(msg, 'ls')
             while eccodes.codes_keys_iterator_next(it):
                 datedata = eccodes.codes_get_string(msg, "dataDate")
                 stepRange = eccodes.codes_get_string(msg, "stepRange")
-
             eccodes.codes_keys_iterator_delete(it)
 
-            # get the lats, lons, values
-            lats = eccodes.codes_get_double_array(msg, "latitudes")
-            # print(lats)
-            lons = eccodes.codes_get_double_array(msg, "longitudes")
-            # print(lons)
-            #values = eccodes.codes_get_double_array(msg, "values")
-            # print(values)
-            #datedata = eccodes.codes_get_double_array(msg, "dataDate")
-            #stepRange = eccodes.codes_get_double_array(msg, "stepRange")
             dateobj = pd.to_datetime(datedata, format='%Y%m%d')
-            forecast_datetime = dateobj + pd.to_timedelta(int(stepRange), unit='h')
-            values_list.append(forecast_datetime)
-            lat_list.append(lats)
-            long_list.append(lons)
+            forecast_datetime = dateobj + \
+                pd.to_timedelta(int(stepRange), unit='h')
+
+            md["date"] = datedata
+            md["step"] = stepRange
+            md["datetime"] = forecast_datetime
+
+            # decode data
+            # get the lats, lons, values
+            md["lats"] = eccodes.codes_get_double_array(msg, "latitudes")
+            # print(lats)
+            md["lons"] = eccodes.codes_get_double_array(msg, "longitudes")
+            # print(lons)
+            md["values"] = eccodes.codes_get_double_array(msg, "values")
+            # print(values)
             eccodes.codes_release(msg)
 
-    f.close()
-    return values_list, lat_list, long_list
+            messages.append(md)
 
+    f.close()
+    return messages
 
 def run_cossza(values):
+    lat = values[1]
+    lon = values[2]
     h = pd.to_datetime(values[0], format='%Y%m%d %H:%m:%s')
+    print(h)
     hours = h.hour
+    print(hours)
     years = h.year
     months = h.month
     days = h.day
-    lon = values[2]
-    lat = values[1]
     base = hours[0]
     step = hours[1]
     c1 = []
@@ -101,10 +68,11 @@ def run_cossza(values):
         for i in range(len(lat)):
             c1.append(
                 calculate_solar_zenith_angle(lat=lat[i], lon=lon[i], y=years[x], m=months[x], d=days[x], h=hours[x]))
-            c2.append(
-                calculate_solar_zenith_angle_f(lat=lat[i], lon=lon[i], y=years[x], m=months[x], d=days[x], h=hours[x], base=base, step=step))
-    print(c1, c2)
-    return(c1,c2)
+            # c2.append(
+            #     calculate_solar_zenith_angle_f(lat=lat[i], lon=lon[i], y=years[x], m=months[x], d=days[x], h=hours[x], base=base, step=step))
+    # print(c1, c2)
+    # return(c1,c2)
+    return c1
 
 
 def run_heatindex(values):
@@ -114,8 +82,10 @@ def run_heatindex(values):
 
 def main():
     try:
-        #run_cossza(decode_csza(sys.argv[1]))
-        run_heatindex(decode_singleparam(sys.argv[1]))
+        v = decode_grib(sys.argv[1])
+        print(v)
+        # run_cossza(v)
+        # run_heatindex(decode_singleparam(sys.argv[1]))
     except eccodes.CodesInternalError as err:
         if eccodes.VERBOSE:
             eccodes.traceback.print_exc(file=sys.stderr)
