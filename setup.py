2a2894aa3885bf6a361dc13c74d6435469184947
Tiago Quintino <tiago.quintino@ecmwf.int>
2021-06-18 10:45:42 +0100

    Add version

--- setup.py
+++ setup.py
@@ -6,4 +6,13 @@
 # granted to it by virtue of its status as an intergovernmental organisation
 # nor does it submit to any jurisdiction.
 
-TODO
+import re
+
+version_file = "thermofeel/_version.py"
+version_line = open(version_file, "rt").read()
+version_regex = r"^__version__ = ['\"]([^'\"]*)['\"]"
+res = re.search(version_regex, version_line, re.M)
+if res:
+    version_string = res.group(1)
+else:
+    raise RuntimeError("Cannot parse version string from {file}.".format(file=version_file))
