38263c22c65588dc0173f5e88a0edbfec9763c4e
Tiago Quintino <tiago.quintino@ecmwf.int>
2021-07-05 19:00:33 +0100

    Code QA

--- tests/test_scalars.py
+++ tests/test_scalars.py
@@ -14,8 +14,8 @@ import pytest
 
 import thermofeel
 
-class TestThermalCalculator(unittest.TestCase):
 
+class TestThermalCalculator(unittest.TestCase):
     def test_relative_humidity_percent(self):
         t2mk = thermofeel.celcius_to_kelvin(30.0)
         t2dk = thermofeel.celcius_to_kelvin(28.0)
@@ -24,19 +24,16 @@ class TestThermalCalculator(unittest.TestCase):
 
         assert rhpc == pytest.approx(89.08526710467393, abs=1e-6)
 
-
     def test_saturation_vapour_pressure(self):
         t2mk = thermofeel.celcius_to_kelvin(25.0)
         svp = thermofeel.calculate_saturation_vapour_pressure(t2mk)
         assert svp == pytest.approx(31.699201897293, abs=1e-6)
-
 
     def test_heat_index(self):
         t2mk = thermofeel.celcius_to_kelvin(25.0)
         hi = thermofeel.calculate_heat_index_simplified(t2mk)
         # print(f"hi {hi}")
         assert hi == pytest.approx(49.321122555, abs=1e-6)
-
 
     # def test_meant_radiant_temperature(self):
     #     ssrd= 15000
@@ -60,7 +57,7 @@ class TestThermalCalculator(unittest.TestCase):
     #      e_hPa = 12
     #      utci= thermofeel.calculate_utci(t2mk,va,mrt,e_hPa)
     #      print(utci)
-    
+
     def test_apparent_temperature(self):
         t2mk = thermofeel.celcius_to_kelvin(25.0)
         va = 3
@@ -68,13 +65,11 @@ class TestThermalCalculator(unittest.TestCase):
         # print(f"at {at}")
         assert at == pytest.approx(27.844072534, abs=1e-6)
 
-
     def test_wbgts(self):
         t2mk = thermofeel.celcius_to_kelvin(30.0)
         wbgts = thermofeel.calculate_wbgts(t2mk)
         # print(f"wbgts {wbgts}")
         assert wbgts == pytest.approx(22.05908916, abs=1e-6)
-
 
     def test_net(self):
         t2mk = 307
@@ -84,7 +79,6 @@ class TestThermalCalculator(unittest.TestCase):
         # print(f"net {net}")
         assert net == pytest.approx(36.0638063, abs=1e-6)
 
-
     def test_humidex(self):
         t2mk = 304
         tdk = 300
@@ -92,13 +86,11 @@ class TestThermalCalculator(unittest.TestCase):
         # print(f"hu {hu}")
         assert hu == pytest.approx(27.97125748479999, abs=1e-6)
 
-
     def test_windchill(self):
         t2mk = 290
         va = 10
         wc = thermofeel.calculate_wind_chill(t2mk, va)
         assert wc == pytest.approx(23.78881163916766, abs=1e-6)
-
 
     def test_heat_index_adjusted(self):
         t2mk = np.array([295])
@@ -107,7 +99,6 @@ class TestThermalCalculator(unittest.TestCase):
         # print(f"hia {hia}")
         assert hia[0] == pytest.approx(22.00355699, abs=1e-6)
 
-
     def test_wbgt(self):
         t2mk = 301
         va = 4
@@ -115,7 +106,6 @@ class TestThermalCalculator(unittest.TestCase):
         wbgt = thermofeel.calculate_wbgt(t2mk, va, mrt)
         # print(f"wbgt {wbgt}")
         assert wbgt == pytest.approx(26.769875412856436, abs=1e-6)
-
 
     def test_calculate_cos_solar_zenith_angle(self):
         # should return ~ 0.360303587797559
@@ -133,13 +123,11 @@ class TestThermalCalculator(unittest.TestCase):
         # from alternative formula
         assert abs(cossza - cos(radians(90.0 - 61.5))) < 1e-2
 
-
     def test_calculate_cos_solar_zenith_angle_integrated(self):
         cossza = thermofeel.calculate_cos_solar_zenith_angle_integrated(
             lat=48.81667, lon=2.28972, d=15, m=11, y=2006, h=10.58333, base=0, step=3
         )
         assert abs(cossza - 0.34495713937581207) < 1e-6
-
 
     def test_solar_declination_angle(self):
         sda1, tc1 = thermofeel.solar_declination_angle(jd=166, h=0)
