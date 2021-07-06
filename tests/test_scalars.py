5587c76090682f2e90fe8de962cbea19838bf611
Tiago Quintino <tiago.quintino@ecmwf.int>
2021-07-06 10:06:21 +0100

    Test dark side of earth

--- tests/test_scalars.py
+++ tests/test_scalars.py
@@ -120,14 +120,15 @@ class TestThermalCalculator(unittest.TestCase):
         cossza = thermofeel.calculate_cos_solar_zenith_angle(
             lat=48.81667, lon=2.28972, d=15, m=11, y=2006, h=10.58333
         )
-        assert abs(cossza - 0.360303587797559) < 1e-6
+        # print(f"cossza {cossza}")
+        assert cossza == pytest.approx(0.360303587797559, abs=1e-6)
 
         # London, ~ 0.8799471697555967
         cossza = thermofeel.calculate_cos_solar_zenith_angle(
             lat=51.0, lon=0.0, d=4, m=6, y=2021, h=12.0
         )
-        assert abs(cossza - 0.8799471697555967) < 1e-6
-
+        # print(f"cossza {cossza}")
+        assert cossza == pytest.approx(0.8799471697555967, abs=1e-6)
         # from alternative formula
         assert abs(cossza - cos(radians(90.0 - 61.5))) < 1e-2
 
@@ -135,7 +136,22 @@ class TestThermalCalculator(unittest.TestCase):
         cossza = thermofeel.calculate_cos_solar_zenith_angle_integrated(
             lat=48.81667, lon=2.28972, d=15, m=11, y=2006, h=10.58333, base=0, step=3
         )
-        assert abs(cossza - 0.34495713937581207) < 1e-6
+        # print(f"cossza {cossza}")
+        assert cossza == pytest.approx(0.34495713937581207, abs=1e-6)
+
+        # opposite point in the world should be dark
+        cossza = thermofeel.calculate_cos_solar_zenith_angle_integrated(
+            lat=-48.81667,
+            lon=180 + 2.28972,
+            d=15,
+            m=11,
+            y=2006,
+            h=10.58333,
+            base=0,
+            step=3,
+        )
+        print(f"cossza {cossza}")
+        assert cossza == pytest.approx(0.0, abs=1e-6)
 
     def test_solar_declination_angle(self):
         sda1, tc1 = thermofeel.solar_declination_angle(jd=166, h=0)
