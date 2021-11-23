a8eec1e26ac71a1e415fde995abf95aff6de7ecd
Tiago Quintino <tiago.quintino@ecmwf.int>
2021-11-23 12:58:02 +0000

    Optimise the call to cossza

--- examples/thermofeel_calculations.py
+++ examples/thermofeel_calculations.py
@@ -6,8 +6,12 @@
 # granted to it by virtue of its status as an intergovernmental organisation
 # nor does it submit to any jurisdiction.
 
+
+import time
 import math
 import sys
+import functools
+
 from datetime import datetime, timedelta, timezone
 
 import eccodes
@@ -15,6 +19,20 @@ import numpy as np
 
 import thermofeel as thermofeel
 
+############################################################################################################
+
+def timer(func):
+    @functools.wraps(func)
+    def wrapper_timer(*args, **kwargs):
+        tic = time.perf_counter()
+        value = func(*args, **kwargs)
+        toc = time.perf_counter()
+        elapsed_time = toc - tic
+        print(f"{func} elapsed time: {elapsed_time:0.6f} s")
+        return value
+    return wrapper_timer
+
+############################################################################################################
 
 def mydebug(name, values):
     print(
@@ -22,6 +40,10 @@ def mydebug(name, values):
         f"min {np.min(values)} stddev {np.std(values, dtype=np.float64)}"
     )
 
+############################################################################################################
+
+lats = None
+lons = None
 
 def decode_grib(fpath):
 
@@ -116,10 +138,17 @@ def decode_grib(fpath):
 
             # decode data
             # get the lats, lons, values
-            md["lats"] = eccodes.codes_get_double_array(msg, "latitudes")
+            # md["lats"] = eccodes.codes_get_double_array(msg, "latitudes")
             # print(lats)
-            md["lons"] = eccodes.codes_get_double_array(msg, "longitudes")
+            # md["lons"] = eccodes.codes_get_double_array(msg, "longitudes")
             # print(lons)
+            global lats
+            if lats is None:
+                lats = eccodes.codes_get_double_array(msg, "latitudes")
+            global lons
+            if lons is None:
+                lons = eccodes.codes_get_double_array(msg, "longitudes")
+
             md["values"] = eccodes.codes_get_double_array(msg, "values")
             # print(values)
 
@@ -132,12 +161,8 @@ def decode_grib(fpath):
     f.close()
 
 
-def calc_cossza_int(messages, begin, end):
-
-    dt = messages["2t"]["base_datetime"]
-    lats = messages["2t"]["lats"]
-    lons = messages["2t"]["lons"]
-    assert lats.size == lons.size
+@timer
+def calc_cossza_int(dt, begin, end):
 
     # print(dt.year, dt.month, dt.day, dt.hour)
 
@@ -150,11 +175,13 @@ def calc_cossza_int(messages, begin, end):
         h=dt.hour,
         tbegin=begin,
         tend=end,
+        integration_order=2,
     )
 
     return integral
 
 
+@timer
 def calc_apparent_temp(messages):
 
     t2m = messages["2t"]["values"]
@@ -167,6 +194,7 @@ def calc_apparent_temp(messages):
     return at
 
 
+@timer
 def calc_humidex(messages):
     t2m = messages["2t"]["values"]
     td = messages["2d"]["values"]
@@ -176,6 +204,7 @@ def calc_humidex(messages):
     return humidex
 
 
+@timer
 def calc_rela_humid_perc(messages):
     t2m = messages["2t"]["values"]
     td = messages["2d"]["values"]
@@ -185,6 +214,7 @@ def calc_rela_humid_perc(messages):
     return rhp
 
 
+@timer
 def calc_mrt(messages, cossza):
 
     step = messages["2t"]["step"]
@@ -209,6 +239,7 @@ def calc_mrt(messages, cossza):
     return mrt
 
 
+@timer
 def calc_utci(messages, mrt):
 
     t2m = messages["2t"]["values"]
@@ -217,13 +248,13 @@ def calc_utci(messages, mrt):
     t2d = messages["2d"]["values"]
 
     va = np.sqrt(u10 ** 2 + v10 ** 2)
-    mydebug("va", va)
+    # mydebug("va", va)
 
     rh_pc = thermofeel.calculate_relative_humidity_percent(t2m, t2d)
-    mydebug("rh_pc", rh_pc)
+    # mydebug("rh_pc", rh_pc)
 
     ehPa = thermofeel.calculate_saturation_vapour_pressure(t2m) * rh_pc / 100.0
-    mydebug("ehPa", ehPa)
+    # mydebug("ehPa", ehPa)
 
     utci = thermofeel.calculate_utci(t2_k=t2m, va_ms=va, mrt_k=mrt, e_hPa=ehPa)
 
@@ -242,21 +273,19 @@ def check_messages(msgs):
     assert "strd" in msgs
 
     # check grids all compatible
-    lats = msgs["2t"]["lats"]
-    lons = msgs["2t"]["lons"]
+    # lats = msgs["2t"]["lats"]
+    # lons = msgs["2t"]["lons"]
 
     assert lats.size == lons.size
 
     ftime = msgs["2t"]["forecast_datetime"]
 
     for k, m in msgs.items():
-        nlats = m["lats"].size
-        nlons = m["lons"].size
-        assert nlats == lats.size
-        assert nlons == lons.size
+        assert lats.size == m["values"].size
         assert ftime == m["forecast_datetime"]
 
 
+@timer
 def output_grib(output, msg, paramid, values, missing=None):
     # encode results in GRIB
     grib = msg["grib"]
@@ -272,8 +301,11 @@ def output_grib(output, msg, paramid, values, missing=None):
 
 def main():
 
+    cossza = None
+
     output = open(sys.argv[2], "wb")
 
+    print(f"----------------------------------------")
     for msgs in decode_grib(sys.argv[1]):
 
         check_messages(msgs)
@@ -282,23 +314,25 @@ def main():
 
         msg = msgs["2t"]
 
-        # dt = msg["base_datetime"]
+        step = msg["step"]
+        time = msg["time"]
+        dt = msgs["2t"]["base_datetime"]
 
-        ftime = int(msg["time"] / 100)
+        ftime = int(time / 100)
 
         step_begin = ftime
-        step_end = ftime + msg["step"]
+        step_end = ftime + step
 
-        # print(f"Date {dt} -- Time {ftime} -- Interval [{step_begin},{step_end}]")
+        print(f"dt {dt.date().isoformat()} time {time} step {step} - [{step_begin},{step_end}]")
 
-        t2 = msgs["2t"]["values"]  # Kelvin
-        mydebug("2t", t2)
+        if cossza is None:
+            print(f"[{step_begin},{step_end}]")
+            cossza = calc_cossza_int(dt=dt, begin=step_begin, end=step_end)        
+        else:
+            print(f"[{last_step_end},{step_end}]")
+            cossza += cossza + calc_cossza_int(dt=dt, begin=last_step_end, end=step_end)
 
-        # humidex = calc_humidex(messages=msgs)
-        # apparenttemp = calc_apparent_temp(messages=msgs)
-        # rhp = calc_rela_humid_perc(messages=msgs)
-
-        cossza = calc_cossza_int(messages=msgs, begin=step_begin, end=step_end)
+        last_step_end = step_end
         # mydebug("cossza", cossza)
 
         mrt = calc_mrt(messages=msgs, cossza=cossza)
@@ -313,8 +347,9 @@ def main():
         # output_grib(output,msg,"260255",apparenttemp)
         # output_grib(output,msg,"260004",humidex) #heat index parameter ID
         output_grib(output, msg, "214001", cossza)
-        output_grib(output, msg, "261001", utci, float(-9999))
+        output_grib(output, msg, "261001", utci)
         output_grib(output, msg, "261002", mrt)
+        print(f"----------------------------------------")
 
 
 if __name__ == "__main__":
