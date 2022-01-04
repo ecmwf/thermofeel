37c4ab9f4c7b98b11373818ce20afa74cfa7b42e
Chloe Brimicombe <Chloe.Brimicombe@ecmwf.int>
2022-01-04 10:08:58 +0000

    addinghiatocalc

--- examples/thermofeel_calculations.py
+++ examples/thermofeel_calculations.py
@@ -171,6 +171,12 @@ def calc_cossza_int(dt, begin, end):
 
     return integral
 
+@thermofeel.timer
+def calc_heat_index_ad(messages):
+    t2m = messages["2t"]["values"]
+    td = messages["2d"]["values"]
+
+    hia = thermofeel.calculate.heat_index_adjusted(t2m=t2m, td=td)
 
 @thermofeel.timer
 def calc_apparent_temp(messages, va):
@@ -357,12 +363,13 @@ def output_grib(output, msg, paramid, values, missing=None):
 
 
 @thermofeel.timer
-def output_gribs(output, msg, cossza, mrt, utci):
+def output_gribs(output, msg, cossza, mrt, utci,apparenttemp,humidex,hia):
 
     # output_grib(output, msg, "167", t2)
     # output_grib(output,msg,"157",rhp)
-    # output_grib(output, msg, "260255", apparenttemp)
-    # output_grib(output,msg,"260004", humidex) #heat index parameter ID
+    output_grib(output, msg, "260255", apparenttemp)
+    output_grib(output,msg,"260005", humidex) #wind chill parameter ID
+    output_grib(output,msg,"26004",hia)
     # output_grib(output, msg, "260005", windchill)
     output_grib(output, msg, "214001", cossza)
     output_grib(output, msg, "261001", utci, missing=MISSING_VALUE)
@@ -410,11 +417,12 @@ def process_step(msgs, output):
     va = calc_va(messages=msgs)
     utci = calc_utci(messages=msgs, mrt=mrt, va=va)
 
-    # humidex = calc_humidex(messages=msgs)
+    humidex = calc_humidex(messages=msgs)
     # windchill = calc_windchill(messages=msgs, va=va)
-    # apparenttemp = calc_apparent_temp(messages=msgs, va=va)
-
-    output_gribs(output=output, msg=msg, cossza=cossza, mrt=mrt, utci=utci)
+    apparenttemp = calc_apparent_temp(messages=msgs, va=va)
+    hia = calc_heat_index_ad(messages=msgs)
+    output_gribs(output=output, msg=msg, cossza=cossza, mrt=mrt, utci=utci,
+                 apparenttemp= apparenttemp, hia=hia, humidex=humidex)
 
 
 def main():
